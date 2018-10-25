3bedfcdb316b74695b68097cb279871afeef2250
Anders Østby <anders.ostby@nav.no>
2018-10-25 10:06:09 +0200

    Legger til logger i Application.kt

--- src/main/kotlin/no/nav/syfo/Application.kt
+++ src/main/kotlin/no/nav/syfo/Application.kt
@@ -1,11 +1,19 @@
 package no.nav.syfo
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
+const val CALL_ID = "callId"
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+}
+
+inline fun <reified T> T.log(): Logger {
+    return LoggerFactory.getLogger(T::class.java)
 }
--- src/main/kotlin/no/nav/syfo/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/syfo/config/ApplicationConfig.kt
@@ -2,7 +2,5 @@ package no.nav.syfo.config
 
 import org.springframework.context.annotation.Configuration
 
-const val CALL_ID = "callId"
-
 @Configuration
 class ApplicationConfig
