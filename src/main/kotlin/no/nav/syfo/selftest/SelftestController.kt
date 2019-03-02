67bf6b59f19dd42fe6be98050f840224023c7bf4
Anders Østby <anders.ostby@nav.no>
2019-03-02 19:29:10 +0100

    Appen restartes hvis tilkobling til kafka feiler for mye

--- src/main/kotlin/no/nav/syfo/selftest/SelftestController.kt
+++ src/main/kotlin/no/nav/syfo/selftest/SelftestController.kt
@@ -1,8 +1,12 @@
 package no.nav.syfo.selftest
 
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 const val APPLICATION_LIVENESS = "Application is alive!"
@@ -10,15 +14,17 @@ const val APPLICATION_READY = "Application is ready!"
 
 @RestController
 @RequestMapping(value = ["/internal"])
-class SelftestController {
+class SelftestController(private val registry: MeterRegistry) {
 
-    val isAlive: String
-        @ResponseBody
-        @RequestMapping(value = ["/isAlive"], produces = [MediaType.TEXT_PLAIN_VALUE])
-        get() = APPLICATION_LIVENESS
+    val isAlive: ResponseEntity<String>
+        @GetMapping(value = ["/isAlive"], produces = [MediaType.TEXT_PLAIN_VALUE])
+        get() = if (registry.counter("syfoaltinn.kafka.feil", Tags.of("type", "fatale")).count() > 2.0) {
+            ResponseEntity("Feil: for mange fatale kafkafeil", HttpStatus.INTERNAL_SERVER_ERROR)
+        } else {
+            ResponseEntity.ok(APPLICATION_LIVENESS)
+        }
 
     val isReady: String
-        @ResponseBody
-        @RequestMapping(value = ["/isReady"], produces = [MediaType.TEXT_PLAIN_VALUE])
+        @GetMapping(value = ["/isReady"], produces = [MediaType.TEXT_PLAIN_VALUE])
         get() = APPLICATION_READY
 }
