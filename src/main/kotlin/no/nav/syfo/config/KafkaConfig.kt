cabc0f50b0d77eff38cc1fcfa26e97df25431e09
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-11-17 07:38:01 +0100

    Les hele topicet (#52)

--- src/main/kotlin/no/nav/syfo/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/syfo/config/KafkaConfig.kt
@@ -10,6 +10,7 @@ import no.nav.syfo.kafka.KafkaErrorHandler
 import no.nav.syfo.kafka.felles.SykepengesoknadDTO
 import no.nav.syfo.kafka.soknad.deserializer.FunctionDeserializer
 import no.nav.syfo.kafka.soknad.serializer.FunctionSerializer
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
@@ -56,6 +57,29 @@ class KafkaConfig(private val properties: KafkaProperties) {
                         containerProperties.setErrorHandler(kafkaErrorHandler)
                         this.consumerFactory = consumerFactory
                     }
+
+    @Bean
+    fun incidentFixKafkaListenerContainerFactory(
+    ): ConcurrentKafkaListenerContainerFactory<String, SykepengesoknadDTO> {
+
+        val configs = properties.buildConsumerProperties()
+        configs["auto.offset.reset"] = "earliest"
+        configs[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = 1000
+
+
+        val consumerFactory = DefaultKafkaConsumerFactory(
+                configs,
+                StringDeserializer(),
+                FunctionDeserializer { bytes -> objectMapper.readValue(bytes, SykepengesoknadDTO::class.java) }
+        )
+
+        return ConcurrentKafkaListenerContainerFactory<String, SykepengesoknadDTO>()
+                .apply {
+                    containerProperties.ackMode = MANUAL_IMMEDIATE
+                    this.consumerFactory = consumerFactory
+                    this.isBatchListener = true
+                }
+    }
 
     @Bean
     @Primary
