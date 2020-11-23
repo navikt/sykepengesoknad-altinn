5d0ad209c9c76eabe51e2188d1b98c8c50c280bb
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2020-11-23 13:48:26 +0100

    Token caches (#53)

--- src/main/kotlin/no/nav/syfo/consumer/rest/token/TokenConsumer.kt
+++ src/main/kotlin/no/nav/syfo/consumer/rest/token/TokenConsumer.kt
@@ -1,5 +1,7 @@
 package no.nav.syfo.consumer.rest.token
 
+import no.nav.syfo.consumer.rest.token.Token.Companion.shouldRenewToken
+import no.nav.syfo.log
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -9,34 +11,69 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
+import java.time.Instant
 import java.util.Objects.requireNonNull
 
 @Component
 class TokenConsumer(private val basicAuthRestTemplate: RestTemplate,
                     @Value("\${security.token.service.token.url}") private val url: String) {
 
+    private val log = log()
+    private var cachedToken: Token? = null
     val token: Token
         get() {
-            val headers = HttpHeaders()
-            headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+            return hentToken()
+        }
 
-            val uriString = UriComponentsBuilder.fromHttpUrl(url)
-                    .queryParam("grant_type", "client_credentials")
-                    .queryParam("scope", "openid")
-                    .toUriString()
+    fun hentToken(): Token {
+        if (shouldRenewToken(cachedToken)) {
+            log.info("Henter nytt token fra STS")
 
-            val result = basicAuthRestTemplate.exchange(uriString, GET, HttpEntity<Any>(headers), Token::class.java)
+            val result = basicAuthRestTemplate.exchange(uriString(), GET, headers(), Token::class.java)
 
             if (result.statusCode != OK) {
                 throw RuntimeException("Henting av token feiler med HTTP-" + result.statusCode)
             }
 
-            return requireNonNull<Token>(result.body)
+            cachedToken = result.body
         }
+
+        return requireNonNull<Token>(cachedToken)
+    }
+
+    private fun uriString(): String {
+        return UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("grant_type", "client_credentials")
+                .queryParam("scope", "openid")
+                .toUriString()
+    }
+
+    private fun headers(): HttpEntity<Any> {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+        return HttpEntity<Any>(headers)
+    }
 }
 
 data class Token(
         val access_token: String,
         val token_type: String,
         val expires_in: Int
-)
+) {
+
+    val expirationTime: Instant = Instant.now().plusSeconds(expires_in - 10L)
+
+    companion object {
+        fun shouldRenewToken(token: Token?): Boolean {
+            if (token == null) {
+                return true
+            }
+            return isExpired(token)
+        }
+
+        private fun isExpired(token: Token): Boolean {
+            return token.expirationTime.isBefore(Instant.now())
+        }
+    }
+}
