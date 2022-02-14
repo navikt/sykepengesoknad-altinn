d88f4f8878736350902d54e3532ee51252d0ca4e
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-02-14 13:13:10 +0100

    Øker concurrency til 3

--- src/main/kotlin/no/nav/syfo/kafka/AivenSykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/syfo/kafka/AivenSykepengesoknadListener.kt
@@ -9,10 +9,7 @@ import no.nav.syfo.kafka.felles.SykepengesoknadDTO
 import no.nav.syfo.logger
 import no.nav.syfo.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.event.ConsumerStoppedEvent
-import org.springframework.kafka.listener.KafkaMessageListenerContainer
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
@@ -29,6 +26,7 @@ class AivenSykepengesoknadListener(
 
     @KafkaListener(
         topics = [SYKEPENGESOKNAD_TOPIC],
+        concurrency = "3",
         containerFactory = "aivenKafkaListenerContainerFactory"
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
@@ -48,21 +46,6 @@ class AivenSykepengesoknadListener(
         }
 
         acknowledgment.acknowledge()
-    }
-
-    @EventListener
-    fun eventHandler(event: ConsumerStoppedEvent) {
-        if (event.reason == ConsumerStoppedEvent.Reason.NORMAL) {
-            return
-        }
-        log.error("Consumer stoppet grunnet ${event.reason}")
-        if (event.source is KafkaMessageListenerContainer<*, *> &&
-            event.reason == ConsumerStoppedEvent.Reason.AUTH
-        ) {
-            val container = event.source as KafkaMessageListenerContainer<*, *>
-            log.info("Trying to restart consumer, creds may be rotated")
-            container.start()
-        }
     }
 
     fun String.tilSykepengesoknadDTO(): SykepengesoknadDTO = objectMapper.readValue(this)
