3d78e8cfd06dde04f43a3516d046ce18850ad882
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-03-04 09:33:11 +0100

    Rydd i dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,2 @@
 FROM navikt/java:17
-COPY init.sh /init-scripts/init.sh
 COPY build/libs/app.jar /app/
-
-ENV JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom \
-               -Dspring.profiles.active=remote"
--- init.sh
+++
@@ -1,6 +0,0 @@
-export SRVSYFOALTINN_USERNAME=$(cat /secrets/serviceuser/username)
-export SRVSYFOALTINN_PASSWORD=$(cat /secrets/serviceuser/password)
-
-export SPRING_DATASOURCE_USERNAME=$(cat /secrets/sykepengesoknad-altinn/credentials/username)
-export SPRING_DATASOURCE_PASSWORD=$(cat /secrets/sykepengesoknad-altinn/credentials/password)
-export SPRING_DATASOURCE_URL=$(cat /secrets/sykepengesoknad-altinn/config/jdbc_url)
