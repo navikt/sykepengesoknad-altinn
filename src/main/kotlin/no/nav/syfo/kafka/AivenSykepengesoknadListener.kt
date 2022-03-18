dd4e1152684123811adb334fd17677b719b20736
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-03-18 07:47:14 +0100

    Starter lytting av sykepengesoknad topic

--- src/main/kotlin/no/nav/syfo/kafka/AivenSykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/syfo/kafka/AivenSykepengesoknadListener.kt
@@ -9,7 +9,6 @@ import no.nav.syfo.SendTilAltinnService
 import no.nav.syfo.logger
 import no.nav.syfo.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
@@ -17,7 +16,6 @@ import org.springframework.stereotype.Component
 const val SYKEPENGESOKNAD_TOPIC = "flex." + "sykepengesoknad"
 
 @Component
-@Profile("test")
 class AivenSykepengesoknadListener(
     private val sendTilAltinnService: SendTilAltinnService,
     private val rebehandleSykepengesoknadProducer: RebehandleSykepengesoknadProducer,
