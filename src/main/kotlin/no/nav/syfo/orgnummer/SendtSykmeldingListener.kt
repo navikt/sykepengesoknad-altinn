61dbbb15e56a4c39791e2aa17957ddafdc214820
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-03-18 08:11:14 +0100

    Fjerner import listener

--- src/main/kotlin/no/nav/syfo/orgnummer/SendtSykmeldingListener.kt
+++ src/main/kotlin/no/nav/syfo/orgnummer/SendtSykmeldingListener.kt
@@ -1,6 +1,7 @@
 package no.nav.syfo.orgnummer
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.syfo.logger
 import no.nav.syfo.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
@@ -11,31 +12,46 @@ const val SYKMELDINGSENDT_TOPIC = "teamsykmelding." + "syfo-sendt-sykmelding"
 
 @Component
 class SendtSykmeldingListener(
-    val juridiskOrgnummerRepository: BatchInsertOrgnummerDAO
+    val juridiskOrgnummerRepository: JuridiskOrgnummerRepository
 ) {
+
+    private val log = logger()
 
     @KafkaListener(
         topics = [SYKMELDINGSENDT_TOPIC],
-        containerFactory = "sendtSykmeldingContainerFactory"
+        containerFactory = "sendtSykmeldingContainerFactory",
+        concurrency = "3"
     )
     fun listen(records: List<ConsumerRecord<String, String>>, acknowledgment: Acknowledgment) {
-        val rader = records
+        records
             .map { it.value().tilSykmeldingKafkaMessage() }
-            .map {
+            .forEach {
                 if (it.event.arbeidsgiver == null) {
-                    null
+                    return@forEach
+                }
+
+                val arbeidsgiver = it.event.arbeidsgiver!!
+                val sykmeldingId = it.event.sykmeldingId
+                val eksisterende = juridiskOrgnummerRepository.findBySykmeldingId(sykmeldingId)
+                if (eksisterende != null) {
+                    if (eksisterende.juridiskOrgnummer != arbeidsgiver.juridiskOrgnummer) {
+                        log.warn("Mismatch mellom eksisterende juridisk orgnummer ${eksisterende.juridiskOrgnummer} og nytt ${arbeidsgiver.juridiskOrgnummer} for sykmelding $sykmeldingId")
+                    }
+                    if (eksisterende.orgnummer != arbeidsgiver.orgnummer) {
+                        log.warn("Mismatch mellom eksisterende orgnummer ${eksisterende.orgnummer} og nytt ${arbeidsgiver.orgnummer} for sykmelding $sykmeldingId")
+                    }
                 } else {
-                    val arbeidsgiver = it.event.arbeidsgiver!!
-                    val sykmeldingId = it.event.sykmeldingId
-                    JuridiskOrgnummer(
-                        id = null,
-                        sykmeldingId = sykmeldingId,
-                        juridiskOrgnummer = arbeidsgiver.juridiskOrgnummer,
-                        orgnummer = arbeidsgiver.orgnummer
+                    juridiskOrgnummerRepository.save(
+                        JuridiskOrgnummer(
+                            id = null,
+                            sykmeldingId = sykmeldingId,
+                            juridiskOrgnummer = arbeidsgiver.juridiskOrgnummer,
+                            orgnummer = arbeidsgiver.orgnummer
+                        )
                     )
                 }
-            }.filterNotNull()
-        juridiskOrgnummerRepository.batchInsertOrgnummer(rader)
+            }
+
         acknowledgment.acknowledge()
     }
 }
