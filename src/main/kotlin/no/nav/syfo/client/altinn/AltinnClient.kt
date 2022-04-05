061057927ca238e551c066efa324538050d191f7
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-04-05 16:05:55 +0200

    Lint

--- src/main/kotlin/no/nav/syfo/client/altinn/AltinnClient.kt
+++ src/main/kotlin/no/nav/syfo/client/altinn/AltinnClient.kt
@@ -5,7 +5,6 @@ import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.Storage
 import no.altinn.schemas.services.intermediary.receipt._2009._10.ReceiptStatusEnum
 import no.altinn.services.serviceengine.correspondence._2009._10.ICorrespondenceAgencyExternalBasic
-import no.altinn.services.serviceengine.correspondence._2009._10.ICorrespondenceAgencyExternalInsertCorrespondenceV2AltinnFaultFaultFaultMessage
 import no.nav.syfo.SoknadAltinnMapper
 import no.nav.syfo.domain.AltinnInnsendelseEkstraData
 import no.nav.syfo.domain.soknad.Sykepengesoknad
