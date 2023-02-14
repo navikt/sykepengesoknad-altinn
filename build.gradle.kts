df057774a715ca7a58882eb57affa7540370e4d3
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2023-02-14 12:56:39 +0100

    Oppgrader til Spring Boot 3 (#145)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.5"
+    id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
-    kotlin("plugin.spring") version "1.8.10"
     kotlin("jvm") version "1.8.10"
+    kotlin("plugin.spring") version "1.8.10"
 }
 
 group = "no.nav.helse.flex"
@@ -29,64 +29,75 @@ repositories {
     }
 }
 
-ext["okhttp3.version"] = "4.9.0" // For at token support testen kjører (tror jeg)
+ext["okhttp3.version"] = "4.9.3" // Token-support tester trenger Mockwebserver.
 
-val sykepengesoknadKafkaVersion = "2022.10.28-10.24-aa0eced7"
+val sykepengesoknadKafkaVersion = "2022.12.21-07.53-20bd43a2"
 val logstashLogbackEncoderVersion = "7.2"
-val tjenestespesifikasjonerVersion = "1.2020.01.20-15.44-063ae9f84815"
-val cxfVersion = "3.5.5"
+val tjenestespesifikasjonerVersion = "2610.9b6de22"
 val testContainersVersion = "1.17.6"
 val kluentVersion = "1.72"
-val tokenSupportVersion = "2.1.9"
+val tokenSupportVersion = "3.0.3"
 val smCommonVersion = "1.1e5e122"
 val gcsVersion = "2.18.0"
 val gcsNioVersion = "0.126.3"
+val commonsTextVersion = "1.10.0"
+val jaxWsApiVersion = "1.1"
+val cxfVersion = "3.5.5"
+val bindApiVersion = "2.3.3"
+val jaxbRuntimeVersion = "2.4.0-b180830.0438"
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("javax.jws:javax.jws-api:1.1")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.hibernate.validator:hibernate-validator")
-    implementation("org.springframework.kafka:spring-kafka")
-    implementation("org.apache.httpcomponents:httpclient")
-    implementation("no.nav.helse.flex:sykepengesoknad-kafka:$sykepengesoknadKafkaVersion")
-    implementation("org.apache.cxf:cxf-spring-boot-starter-jaxws:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-ws-policy:$cxfVersion")
-    implementation("com.sun.xml.messaging.saaj:saaj-impl:2.0.1")
-    implementation("javax.xml.soap:saaj-api:1.3.5")
-    implementation("javax.xml.ws:jaxws-api:2.3.1")
-    implementation("org.apache.commons:commons-text:1.10.0")
-    implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
-    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
-    implementation("no.nav.helse:syfosm-common-models:$smCommonVersion")
-    implementation("com.google.cloud:google-cloud-storage:$gcsVersion")
-
-    implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.hibernate.validator:hibernate-validator")
     implementation("org.postgresql:postgresql")
     implementation("org.flywaydb:flyway-core")
     implementation("org.slf4j:slf4j-api")
     implementation("org.aspectj:aspectjrt")
     implementation("org.aspectj:aspectjweaver")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("org.apache.httpcomponents.client5:httpclient5")
+
+    implementation("org.apache.cxf:cxf-spring-boot-starter-jaxws:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-ws-policy:$cxfVersion")
+
+    // https://stackoverflow.com/questions/71313332/exception-java-lang-noclassdeffounderror-javax-activation-datahandler
+    implementation("com.sun.mail:jakarta.mail:2.0.1")
+    implementation("javax.xml.soap:saaj-api:1.3.5")
+    implementation("com.sun.xml.messaging.saaj:saaj-impl:3.0.0")
+    // https://stackoverflow.com/questions/71095913/what-is-the-difference-between-jaxb-impl-and-jaxb-runtime
+    implementation("jakarta.xml.bind:jakarta.xml.bind-api:$bindApiVersion")
+    implementation("jakarta.xml.ws:jakarta.xml.ws-api:$bindApiVersion")
+    implementation("org.glassfish.jaxb:jaxb-runtime:$jaxbRuntimeVersion")
+
+    implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
+    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
+    implementation("no.nav.helse:syfosm-common-models:$smCommonVersion")
+    implementation("com.google.cloud:google-cloud-storage:$gcsVersion")
+    implementation("org.apache.commons:commons-text:$commonsTextVersion")
+    implementation("no.nav.helse.flex:sykepengesoknad-kafka:$sykepengesoknadKafkaVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
     implementation("no.nav.tjenestespesifikasjoner:behandle-altinnmelding-v1-tjenestespesifikasjon:$tjenestespesifikasjonerVersion")
     implementation("no.nav.tjenestespesifikasjoner:digisyfo-sykepengesoeknadarbeidsgiver:$tjenestespesifikasjonerVersion")
     implementation("no.nav.tjenestespesifikasjoner:altinn-correspondence-agency-external:$tjenestespesifikasjonerVersion")
 
+    testImplementation(platform("org.testcontainers:testcontainers-bom:$testContainersVersion"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:kafka")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.awaitility:awaitility")
     testImplementation("com.google.cloud:google-cloud-nio:$gcsNioVersion")
     testImplementation("no.nav.security:token-validation-spring-test:$tokenSupportVersion")
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:kafka:$testContainersVersion")
-    testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
-    testImplementation("org.testcontainers:postgresql:$testContainersVersion")
-    testImplementation("org.awaitility:awaitility")
 }
 
 tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
