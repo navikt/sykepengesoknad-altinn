5e2c2d8193dbac3bd2cab0a80bf1474bb8812246
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-03-13 08:13:27 +0100

    Ikke send spørsmål til arbeigsgiver

--- src/test/kotlin/no/nav/syfo/domain/pdf/PDFSoknadTest.kt
+++ src/test/kotlin/no/nav/syfo/domain/pdf/PDFSoknadTest.kt
@@ -53,4 +53,32 @@ class PDFSoknadTest {
 
         assertThat(pdf.sporsmal.size).isEqualTo(1)
     }
+
+    @Test
+    fun `filtrerer bort utenlandsk sykmelding spm`() {
+        val sykepengesoknad = Sykepengesoknad(
+            id = "abc",
+            fnr = "123",
+            type = Soknadstype.ARBEIDSTAKERE,
+            status = Soknadsstatus.SENDT,
+            arbeidsgiver = Arbeidsgiver("abc", "123"),
+            sporsmal = listOf(
+                Sporsmal(
+                    tag = "UTENLANDSK_SYKMELDING_BOSTED"
+                ),
+                Sporsmal(
+                    tag = "UTENLANDSK_SYKMELDING_TRYGD_UTENFOR_NORGE"
+                ),
+                Sporsmal(
+                    tag = "UTENLANDSK_SYKMELDING_LONNET_ARBEID_UTENFOR_NORGE"
+                ),
+                Sporsmal(
+                    tag = "ANSVARSERKLARING"
+                )
+            )
+        )
+        val pdf = PDFSoknad(sykepengesoknad, "123", "abc")
+
+        assertThat(pdf.sporsmal.size).isEqualTo(1)
+    }
 }
