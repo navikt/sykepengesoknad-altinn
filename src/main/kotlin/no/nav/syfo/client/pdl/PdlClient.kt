a058f59662357d3cada9b8e6b17f53d988a9b7ba
oivindkulsrud <oivindkulsrud@gmail.com>
2023-11-01 13:24:44 +0100

    Legger til behandlingsnummer i pdl kall

--- src/main/kotlin/no/nav/syfo/client/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/syfo/client/pdl/PdlClient.kt
@@ -20,6 +20,8 @@ class PdlClient(
 
     private val TEMA = "Tema"
     private val TEMA_SYK = "SYK"
+    private val BEHANDLINGSNUMMER_KEY = "Behandlingsnummer"
+    private val BEHANDLINGSNUMMER_VALUE = "B128"
     private val IDENT = "ident"
     private val HENT_PERSON_QUERY =
         """
@@ -44,7 +46,7 @@ query(${"$"}ident: ID!){
         val responseEntity = pdlRestTemplate.exchange(
             "$pdlApiUrl/graphql",
             HttpMethod.POST,
-            HttpEntity(requestToJson(graphQLRequest), createHeaderWithTema()),
+            HttpEntity(requestToJson(graphQLRequest), createHeaderWithTemaAndBehandlingsnummer()),
             String::class.java
         )
 
@@ -61,9 +63,10 @@ query(${"$"}ident: ID!){
         throw FunctionalPdlError("Fant ikke person, ingen body eller data. ${parsedResponse.hentErrors()}")
     }
 
-    private fun createHeaderWithTema(): HttpHeaders {
+    private fun createHeaderWithTemaAndBehandlingsnummer(): HttpHeaders {
         val headers = createHeader()
         headers[TEMA] = TEMA_SYK
+        headers[BEHANDLINGSNUMMER_KEY] = BEHANDLINGSNUMMER_VALUE
         return headers
     }
 
--- src/test/kotlin/no/nav/syfo/client/pdl/PdlClientTest.kt
+++ src/test/kotlin/no/nav/syfo/client/pdl/PdlClientTest.kt
@@ -22,6 +22,8 @@ class PdlClientTest : Testoppsett() {
 
         responseData `should be equal to` "Ole Gunnar"
         val request = pdlMockWebserver.takeRequest()
+        request.headers["Behandlingsnummer"] `should be equal to` "B128"
+        request.headers["Tema"] `should be equal to` "SYK"
         request.body.readUtf8() `should be equal to` "{\"query\":\"\\nquery(\$ident: ID!){\\n  hentPerson(ident: \$ident) {\\n  \\tnavn(historikk: false) {\\n  \\t  fornavn\\n  \\t  mellomnavn\\n  \\t  etternavn\\n    }\\n  }\\n}\\n\",\"variables\":{\"ident\":\"12345678901\"}}"
 
         request.headers["Authorization"]!!.shouldStartWith("Bearer ey")
