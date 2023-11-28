de5b2a138ab45ebaf3eb56b947005e5f6ed80e0a
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-11-28 13:10:31 +0100

    Revert "Tester i dev"
    This reverts commit 57a6fbb386a51b6d8d9b5264f547fe144a34afa5.

--- src/main/kotlin/no/nav/syfo/kafka/DTOListener.kt
+++ src/main/kotlin/no/nav/syfo/kafka/DTOListener.kt
@@ -20,8 +20,7 @@ class DTOListener(
         "ae41945a-7558-3ac5-bf9f-ef9aa945c1fd",
         "86f17513-f341-3dd0-8f49-f28305a43ba3",
         "6d10f89c-ef88-3fa1-a8a2-490940951cf6",
-        "26945bb5-0414-308b-b634-f33f0d0858a0",
-        "5839c930-904f-344d-a641-82e53dab86bb"
+        "26945bb5-0414-308b-b634-f33f0d0858a0"
     )
 
     @KafkaListener(
@@ -33,8 +32,6 @@ class DTOListener(
         properties = ["auto.offset.reset=earliest"]
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
-        log.info("Soknad ${cr.key()}")
-
         if (cr.key() in soknadIder) {
             log.info("Mottok soknad ${cr.key()} med på kafka")
             aivenSykepengesoknadListener.listen(cr, acknowledgment)
